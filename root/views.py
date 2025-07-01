63061ece6db8d928c4a3a3a6d808df7f362bcbe7
Narayana-1918 <166599902+Narayana-1918@users.noreply.github.com>
2025-07-01 12:24:42 +0530

    Update views.py

--- root/views.py
+++ root/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req,'home.html')
+
+def v1(req):
+    data='django project'
+    return render(req,'v1.html',{'data':data})
